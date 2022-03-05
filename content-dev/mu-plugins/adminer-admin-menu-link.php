4ca2d4c3ad024beae394bfb26b6d6f6d42d6a91f
tricarte <sonagirbas@gmail.com>
2022-03-05 09:38:54 +0300

    change dashicon

--- content-dev/mu-plugins/adminer-admin-menu-link.php
+++ content-dev/mu-plugins/adminer-admin-menu-link.php
@@ -3,6 +3,6 @@
  * Top level menu link to Adminer
  */
 function adminer_menu_link() {
-    add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=root&db=' . get_bloginfo( 'name' ) , '', 'dashicons-editor-table', 22 );
+    add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=root&db=' . get_bloginfo( 'name' ) , '', 'dashicons-database', 22 );
 }
 add_action( 'admin_menu', 'adminer_menu_link' );
