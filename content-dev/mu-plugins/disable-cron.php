0e796a59f245d324854e1ff80815436306a713b6
tricarte <sonagirbas@gmail.com>
2022-03-09 10:04:26 +0300

    add comment

--- content-dev/mu-plugins/disable-cron.php
+++ content-dev/mu-plugins/disable-cron.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Why disable it? Because WP checks cron jobs on every page visit. After
+disabling it, you have to run wp-cron manually. For example auto update wp core
+does not work after this setting nor ’wp-updates-notifier’ plugin. But that
+plugin has system cron support. Requesting https://example.com/wp-cron.php runs
+cron jobs even if the above constant is set true.
+ */
 add_action( 'plugins_loaded', function() {
     if ( defined( 'DOING_CRON' ) && DOING_CRON && php_sapi_name() != 'cli' )
         die();
