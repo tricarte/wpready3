301743ac5ea214eb38047a25d48a5f41f5ef552a
tricarte <sonagirbas@gmail.com>
2022-03-22 11:26:31 +0300

    display adminer link only in development

--- content-dev/mu-plugins/adminer-admin-menu-link.php
+++ content-dev/mu-plugins/adminer-admin-menu-link.php
@@ -3,6 +3,8 @@
  * Top level menu link to Adminer
  */
 function adminer_menu_link() {
-    add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=root&db=' . get_bloginfo( 'name' ) , '', 'dashicons-database', 22 );
+    if ( defined('WP_ENVIRONMENT_TYPE') && 'development' == WP_ENVIRONMENT_TYPE) {
+        add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=root&db=' . get_bloginfo( 'name' ) , '', 'dashicons-database', 22 );
+    }
 }
 add_action( 'admin_menu', 'adminer_menu_link' );
