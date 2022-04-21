3899a6a42aac47cd3643f2b40d9d425d4a700245
tricarte <sonagirbas@gmail.com>
2022-04-21 16:29:18 +0300

    remove script version numbers from urls

---
+++ content-dev/mu-plugins/remove-script-versions-from-urls.php
@@ -0,0 +1,11 @@
+<?php
+
+// remove version from scripts and styles
+function remove_version_scripts_styles($src) {
+    if (strpos($src, 'ver=')) {
+        $src = remove_query_arg('ver', $src);
+    }
+    return $src;
+}
+add_filter('style_loader_src', 'remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'remove_version_scripts_styles', 9999);
