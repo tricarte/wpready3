b7fe2559a21d5f3a29b3e08fc855c84b80cebe18
tricarte <sonagirbas@gmail.com>
2022-05-30 15:16:44 +0300

    fix wpcli cannot check major updates

--- content-dev/mu-plugins/disable-update-notifications.php
+++ content-dev/mu-plugins/disable-update-notifications.php
@@ -4,6 +4,12 @@ function wpr_remove_core_updates(){
     global $wp_version;
     return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
-add_filter('pre_site_transient_update_core','wpr_remove_core_updates'); //hide updates for WordPress itself
-add_filter('pre_site_transient_update_plugins','wpr_remove_core_updates'); //hide updates for all plugins
-add_filter('pre_site_transient_update_themes','wpr_remove_core_updates'); //hide updates for all themes
+
+if('cli' != php_sapi_name()){
+    // Hide updates for WordPress core
+    add_filter('pre_site_transient_update_core','wpr_remove_core_updates');
+    // Hide updates for plugins
+    add_filter('pre_site_transient_update_plugins','wpr_remove_core_updates');
+    // Hide updates for themes
+    add_filter('pre_site_transient_update_themes','wpr_remove_core_updates');
+}
