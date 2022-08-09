332497f7ce97296b67b7d33a0a3004bd1a5bcf68
tricarte <sonagirbas@gmail.com>
2022-08-09 10:51:05 +0300

    remove favicon.php muplugin

--- content-dev/mu-plugins/favicon.php
+++
@@ -1,9 +0,0 @@
-<?php
-function hook_favicon() {
-?>
-<link rel="shortcut icon" type="image/png" href="/favicon.png">
-<link rel="shortcut icon" sizes="192x192" href="/favicon.png">
-<link rel="apple-touch-icon" href="/favicon.png">
-<?php
-}
-add_action('wp_head', 'hook_favicon');
