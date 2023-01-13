11b480e3c540e189c51ab2db6abd18d092124a40
tricarte <sonagirbas@gmail.com>
2023-01-13 17:38:34 +0300

    run playground plugin only on development env

--- content-dev/plugins/wpready-playground/wpready-playground.php
+++ content-dev/plugins/wpready-playground/wpready-playground.php
@@ -10,42 +10,44 @@ License:
 License URI:
 */
 
-// http://local.test/playground
-add_filter( 'generate_rewrite_rules', function ( $wp_rewrite ) {
-    $wp_rewrite->rules = array_merge(
-        ['playground/?$' => 'index.php?playground=1'],
-        $wp_rewrite->rules
-    );
-} );
+if (defined('WP_ENVIRONMENT_TYPE') && 'development' == WP_ENVIRONMENT_TYPE) {
+    // http://local.test/playground
+    add_filter('generate_rewrite_rules', function ($wp_rewrite) {
+        $wp_rewrite->rules = array_merge(
+            ['playground/?$' => 'index.php?playground=1'],
+            $wp_rewrite->rules
+        );
+    });
 
-add_filter( 'query_vars', function( $query_vars ) {
-    $query_vars[] = 'playground';
-    return $query_vars;
-} );
+    add_filter('query_vars', function ($query_vars) {
+        $query_vars[] = 'playground';
+        return $query_vars;
+    });
 
-add_action( 'template_redirect', function() {
-    $playground = intval( get_query_var( 'playground' ) );
-    if ( $playground ) {
-        echo 'Edit plugins/wpready-playground/wpready-playground.php file to modify this content.';
+    add_action('template_redirect', function () {
+        $playground = intval(get_query_var('playground'));
+        if ($playground) {
+            echo 'Edit plugins/wpready-playground/wpready-playground.php file to modify this content.';
 
 ?>
-<h2>Here are all the constants that are defined:</h2>
-    <pre><?php
-        print_r(get_defined_constants());
-?>
+            <h2>Here are all the constants that are defined:</h2>
+            <pre><?php
+                    print_r(get_defined_constants());
+                    ?>
     </pre><?php
-        die;
-    }
-} );
+            die;
+        }
+    });
 
-// Add menu link to the playground
-function playground_menu_link() {
-    add_menu_page( 'Playground', 'Playground', 'edit_posts', WP_HOME . '/playground', '', 'dashicons-editor-code', 22 );
+    // Add menu link to the playground
+    function playground_menu_link() {
+        add_menu_page('Playground', 'Playground', 'edit_posts', WP_HOME . '/playground', '', 'dashicons-editor-code', 22);
+    }
+    add_action('admin_menu', 'playground_menu_link');
 }
-add_action( 'admin_menu', 'playground_menu_link' );
 
 function playground_flush() {
     flush_rewrite_rules();
 }
-register_activation_hook( __FILE__, 'playground_flush' );
-register_deactivation_hook( __FILE__, 'playground_flush' );
+register_activation_hook(__FILE__, 'playground_flush');
+register_deactivation_hook(__FILE__, 'playground_flush');
