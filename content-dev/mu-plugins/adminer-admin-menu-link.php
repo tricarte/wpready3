c17b6dd6ef2fdac09ba108847d402bacb08f7d0f
tricarte <sonagirbas@gmail.com>
2023-01-13 17:41:08 +0300

    change root to admin in adminer menu link

--- content-dev/mu-plugins/adminer-admin-menu-link.php
+++ content-dev/mu-plugins/adminer-admin-menu-link.php
@@ -4,7 +4,7 @@
  */
 function adminer_menu_link() {
     if ( defined('WP_ENVIRONMENT_TYPE') && 'development' == WP_ENVIRONMENT_TYPE && is_dir(ABSPATH . '../adminer')) {
-        add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=root&db=' . get_bloginfo( 'name' ) , '', 'dashicons-database', 22 );
+        add_menu_page( 'Adminer', 'Adminer', 'edit_posts', WP_HOME . '/adminer/?username=admin&db=' . get_bloginfo( 'name' ) , '', 'dashicons-database', 22 );
     }
 }
 add_action( 'admin_menu', 'adminer_menu_link' );
