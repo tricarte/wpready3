30dd574bfa25bbf9a683f0baedcc3d2b37ca7de5
tricarte <sonagirbas@gmail.com>
2024-06-24 15:57:43 +0300

    use avif for jpegs

--- content-dev/mu-plugins/enable-webp.php
+++ content-dev/mu-plugins/enable-webp.php
@@ -1,8 +1,9 @@
 <?php
 add_filter( 'image_editor_output_format', function( $formats ) {
-        $formats['image/jpeg'] = 'image/webp';
-        $formats['image/png'] = 'image/webp';
-        $formats['image/bmp'] = 'image/webp';
+    // Write now, using avif for jpegs
+    // $formats['image/jpeg'] = 'image/webp';
+    $formats['image/png'] = 'image/webp';
+    $formats['image/bmp'] = 'image/webp';
 
-        return $formats;
+    return $formats;
 } );
